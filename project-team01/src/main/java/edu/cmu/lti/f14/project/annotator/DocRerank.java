814eb8f75d6555d3f6af7017134c2598d46eb96e
chouclee <chouclee@cmu.edu>
2014-11-29 17:08:07 -0500

    add bm25

---
+++ project-team01/src/main/java/edu/cmu/lti/f14/project/annotator/DocRerank.java
@@ -0,0 +1,132 @@
+package edu.cmu.lti.f14.project.annotator;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.FSList;
+import org.apache.uima.jcas.cas.TOP;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import util.Utils;
+import com.aliasi.tokenizer.TokenizerFactory;
+
+import edu.cmu.lti.f14.project.docstore.CollectionStat;
+import edu.cmu.lti.oaqa.type.retrieval.AtomicQueryConcept;
+import edu.cmu.lti.oaqa.type.retrieval.ComplexQueryConcept;
+import edu.cmu.lti.oaqa.type.retrieval.Document;
+
+
+public class DocRerank extends JCasAnnotator_ImplBase {
+  
+  private CollectionStat stat;
+
+  /**
+   * Perform initialization logic. Initialize the service.
+   * 
+   * @param aContext
+   *          the UimaContext object
+   */
+  public void initialize(UimaContext aContext) throws ResourceInitializationException {
+    super.initialize(aContext);
+    //this.stat = QueryDocAnnotator.stat;
+  }
+
+  @Override
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    FSIterator<TOP> DocIter = aJCas.getJFSIndexRepository().getAllIndexedFS(Document.type);
+    FSIterator<TOP> queryIter = aJCas.getJFSIndexRepository().getAllIndexedFS(
+            ComplexQueryConcept.type);
+
+    ComplexQueryConcept query = (ComplexQueryConcept) queryIter.next();
+    FSList fslist = query.getOperatorArgs();
+    ArrayList<AtomicQueryConcept> arraylist = Utils
+      .fromFSListToCollection(fslist, AtomicQueryConcept.class);
+
+    // content in the query of each AtomicQuery
+    String queryWOOp = arraylist.get(0).getText();
+
+    //TfIdfDistance tfIdf = new TfIdfDistance(REFINED_TKFACTORY);
+    //tfIdf.handle(queryWOOp);
+
+    List<Document> docList = new ArrayList<Document>();
+    while (DocIter.hasNext()) {
+      Document doc = (Document) DocIter.next();
+      if (doc.getText() == null || doc.getText().trim().length() == 0) {
+        continue;
+      }
+
+      //tfIdf.handle(doc.getText());
+
+      docList.add(doc);
+    }
+
+    for (int i = 0; i < docList.size(); ++i) {
+      Document doc = docList.get(i);
+      //double sim = tfIdf.proximity(queryWOOp, docList.get(i).getText());
+      double bm25 = edu.cmu.lti.f14.project.docstore.Similartiy.BM25(
+              QueryDocAnnotator.stat, queryWOOp, Integer.parseInt(doc.getDocId()));
+      doc.setScore(bm25);
+    }
+
+    Collections.sort(docList, new DocSimComparator());
+    for (int i = 0; i < docList.size(); ++i) {
+      docList.get(i).setRank(i);
+    }
+  }
+
+  /**
+   * Read file through stream LPDictExactNERAnnotator
+   * 
+   * @param filePath
+   *          the file path
+   * @return the string of the file
+   * @throws ResourceInitializationException
+   */
+  private String getFileAsStream(String filePath) throws ResourceInitializationException {
+    StringBuilder sb = new StringBuilder();
+    try {
+      InputStream is = DocRerank.class.getClassLoader().getResourceAsStream(filePath);
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(is, "utf-8"));
+
+      String line = br.readLine();
+      while (line != null) {
+        sb.append(line);
+        sb.append("\n");
+        line = br.readLine();
+      }
+      br.close();
+    } catch (Exception ex) {
+      System.out.println("[Error]: Look Below.");
+      ex.printStackTrace();
+      System.out.println("[Error]: Look Above.");
+      throw new ResourceInitializationException();
+    }
+
+    String content = sb.toString();
+    return content;
+  }
+
+  class DocSimComparator implements Comparator<Document> {
+    @Override
+    public int compare(Document lhs, Document rhs) {
+      if (lhs.getScore() < rhs.getScore()) {
+        return 1;
+      } else if (lhs.getScore() > rhs.getScore()) {
+        return -1;
+      } else {
+        return lhs.getRank() - rhs.getRank();
+      }
+    }
+  }
+}
---
+++ project-team01/src/main/java/edu/cmu/lti/f14/project/docstore/CollectionStat.java
@@ -0,0 +1,88 @@
+package edu.cmu.lti.f14.project.docstore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+import util.StanfordLemmatizer;
+import edu.cmu.lti.oaqa.type.retrieval.Document;
+
+public class CollectionStat {
+  public Map<String, Integer> ctfMap;
+  public Map<String, List<Integer>> invList;
+  public Map<Integer, DocInfo> docInfoMap;
+  public Map<String, Integer> docFreqMap;
+  //public List<Integer> docList;
+  public int N; // corpus size
+  public long collectionLength;
+  
+  class DocInfo {
+    public String url;
+    public int docId;
+    public String title;
+    public int docLen;
+    public Map<String, Integer> tfMap;
+    public DocInfo(Document d) {
+      tfMap = new HashMap<String, Integer>();
+      url = d.getUri();
+      docId = Integer.parseInt(d.getDocId());
+      title = d.getTitle();
+      String text = d.getText();
+      text = StanfordLemmatizer.process(text);
+      text = text.replace(".", "");
+      if (text != "") {
+        String[] terms = text.split("\\s+");
+        docLen = terms.length;
+        Integer tf;
+        for (String term : terms) {
+          tf = tfMap.get(term);
+          if (tf != null)
+            tfMap.put(term, tf + 1);
+          else
+            tfMap.put(term, 1);
+        }
+      }
+    }
+  }
+  
+  public CollectionStat() {
+    ctfMap = new HashMap<String, Integer>();
+    //docList = new ArrayList<DocInfo>();
+    invList = new HashMap<String, List<Integer>>();
+    docInfoMap = new HashMap<Integer, DocInfo>();
+    docFreqMap = new HashMap<String, Integer>();
+    N = 0;
+    collectionLength = 0;
+  }
+  
+  public void addDoc(Document doc) {
+    N++;
+    DocInfo docInfo = new DocInfo(doc);
+    docInfoMap.put(docInfo.docId, docInfo);
+    //docList.add(docInfo);
+    
+    Integer ctf, tf, df;
+    for (String term : docInfo.tfMap.keySet()) {
+      List<Integer> docList = invList.get(term);
+      if (docList == null) 
+        docList = new ArrayList<Integer>();
+      docList.add(docInfo.docId);
+      invList.put(term, docList);
+      
+      ctf = ctfMap.get(term);
+      tf = docInfo.tfMap.get(term);
+      collectionLength += tf;
+      if (ctf != null)
+        ctfMap.put(term, ctf + tf );
+      else
+        ctfMap.put(term, tf);
+      
+      df = docFreqMap.get(term);
+      if (df == null)
+        docFreqMap.put(term, 1);
+      else
+        docFreqMap.put(term, df + 1);
+    }
+  }
+}
